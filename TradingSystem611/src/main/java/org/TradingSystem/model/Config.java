f8812ea5594a82a62b8640ff3676cb859d734bfa
shangzhouyin <cxysz8246@163.com>
2023-04-22 20:06:15 -0400

    ShangzhouPath update

--- TradingSystem611/src/main/java/org/TradingSystem/model/Config.java
+++ TradingSystem611/src/main/java/org/TradingSystem/model/Config.java
@@ -2,4 +2,5 @@ package org.TradingSystem.model;
 
 public class Config {
     public static final String DBPATH  = "jdbc:sqlite:TradingSystem611\\src\\main\\java\\org\\TradingSystem\\database\\chinook.db";
+    public static final String SZPATH =  "jdbc:sqlite:src/main/java/org/TradingSystem/database/chinook.db";
 }
