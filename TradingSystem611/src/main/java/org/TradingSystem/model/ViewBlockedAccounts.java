dfcf99b8ea999266bf944b125fc2e559859d8ec9
shangzhouyin <cxysz8246@163.com>
2023-05-03 16:06:40 -0400

    update all

--- TradingSystem611/src/main/java/org/TradingSystem/model/ViewBlockedAccounts.java
+++ TradingSystem611/src/main/java/org/TradingSystem/model/ViewBlockedAccounts.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 
 public class ViewBlockedAccounts extends JFrame implements ActionListener {
 
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
