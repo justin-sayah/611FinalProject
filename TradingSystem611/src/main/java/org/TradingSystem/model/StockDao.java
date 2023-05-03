a6b3fe2213f4d6b0a5731b71c9d2d465faf03ef2
Justin Sayah <55928969+justin-sayah@users.noreply.github.com>
2023-05-03 15:41:38 -0400

    implemented blocking, not integrated

--- TradingSystem611/src/main/java/org/TradingSystem/model/StockDao.java
+++ TradingSystem611/src/main/java/org/TradingSystem/model/StockDao.java
@@ -22,6 +22,7 @@ public class StockDao {
         return stockDao;
     }
 
+    //gets the stock, whether it is blocked or unblocked
     public Stock getStock(int stockId){
         try{
             String sql = "SELECT * FROM stocks WHERE stockId = ? ";
@@ -32,6 +33,16 @@ public class StockDao {
             ResultSet rs = pstmt.executeQuery();
             if(rs.next()){
                 return new Stock(stockId, rs.getDouble("price"), rs.getString("name"), rs.getString("ticker") );
+            } else{
+                String sql2 = "SELECT * FROM stocks WHERE stockId = ? ";
+
+                PreparedStatement pstmt2 = connection.prepareStatement(sql);
+                pstmt.setInt(1, stockId);
+
+                ResultSet rs2 = pstmt.executeQuery();
+                if(rs.next()){
+                    return new Stock(stockId, rs.getDouble("price"), rs.getString("name"), rs.getString("ticker") );
+                }
             }
         }catch (Exception e){
             System.out.println(e.toString());
@@ -39,7 +50,17 @@ public class StockDao {
         return null;
     }
 
+    //Returns all unblocked and blocked stocks
     public List<Stock> getAllStocks(){
+        List<Stock> allUnblocked = getAllUnblockedStocks();
+        List<Stock> allBlocked = getAllBlockedStocks();
+
+        allUnblocked.addAll(allBlocked);
+        return allUnblocked;
+    }
+
+    //returns only unblocked stocks
+    public List<Stock> getAllUnblockedStocks(){
         try{
             String sql = "SELECT * FROM stocks";
 
@@ -58,6 +79,27 @@ public class StockDao {
         return null;
     }
 
+    //returns only blocked stocks
+    public List<Stock> getAllBlockedStocks(){
+        try{
+            String sql = "SELECT * FROM blockedStocks";
+
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            ArrayList<Stock> stocks = new ArrayList<Stock>();
+            while(rs.next()){
+                stocks.add(new Stock(rs.getInt("stockId"), rs.getDouble("price"), rs.getString("name"), rs.getString("ticker")));
+            }
+            return stocks;
+        }catch (Exception e){
+            System.out.println(e.toString());
+        }
+        return null;
+    }
+
+    //adds a stock to unblocked list
     public void addStock(String name, double price, String ticker){
         String sql = "INSERT INTO stocks (name, price, ticker)" +
                 "VALUES (?,?,?)";
@@ -73,6 +115,7 @@ public class StockDao {
             System.out.println(e.toString());
         }
     }
+    //adds a stock to unlbocked list
     public void addStock(Stock stock){
         String sql = "INSERT INTO stocks (stockId, name, " +
                 "price, ticker)" +
@@ -90,7 +133,67 @@ public class StockDao {
             System.out.println(e.toString());
         }
     }
+
+    //removes a stock from unblocked list and moves it to blocked list
+    public void blockStock(Stock stock){
+
+        //first remove from stocks table
+        removeFromUnblocked(stock.getSecurityId());
+
+        //then add to blockedStock table
+        String sql = "INSERT INTO blockedStocks (stockId, name, " +
+                "price, ticker)" +
+                "VALUES (?,?,?,?)";
+
+        try{
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setInt(1,stock.getSecurityId());
+            pstmt.setString(2, stock.getName());
+            pstmt.setDouble(3, stock.getPrice());
+            pstmt.setString(4, stock.getTicker());
+
+            pstmt.executeUpdate();
+        } catch (Exception e){
+            System.out.println(e.toString());
+        }
+    }
+
+    public void blockStock(int stockId){
+        Stock toBlock = getStock(stockId);
+        blockStock(toBlock);
+    }
+
+    //removes a stock from blocked list and moves it back to unblocked list
+    public void unblockStock(Stock stock){
+        //add to unblocked stocks
+        addStock(stock);
+        removeFromBlocked(stock.getSecurityId());
+    }
+
+    public void unblockStock(int stockId){
+        //get the stock
+        Stock toUnblock = getStock(stockId);
+
+        //add to unblocked stocks
+        addStock(toUnblock);
+        //remove from blocked
+        removeFromBlocked(toUnblock.getSecurityId());
+    }
+
+    //removes a stock from both blocked and unblocked list
     public void deleteStock(int stockId){
+        //TODO: Sell positions for all people that own this stock
+        removeFromUnblocked(stockId);
+        removeFromBlocked(stockId);
+    }
+
+    //removes stock from unblocked and blocked list
+    public void deleteStock(Stock stock){
+        deleteStock(stock.getSecurityId());
+    }
+
+    //deletes a stock from unblocked list
+    public void removeFromUnblocked(int stockId){
         String sql = "DELETE FROM stocks WHERE stockId = ?";
 
         try{
@@ -103,8 +206,37 @@ public class StockDao {
         }
     }
 
-    public void deleteStock(Stock stock){
-        deleteStock(stock.getSecurityId());
+    public boolean isBlocked(int stockId){
+        try{
+            String sql = "SELECT * FROM stocks WHERE stockId = ? ";
+
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setInt(1, stockId);
+
+            ResultSet rs = pstmt.executeQuery();
+            if(rs.next()){
+                return false;
+            } else{
+                return true;
+            }
+        }catch (Exception e){
+            System.out.println(e.toString());
+        }
+        return false;
+    }
+
+    //deletes a stock from blocked list
+    public void removeFromBlocked(int stockId){
+        String sql = "DELETE FROM blockedStocks WHERE stockId = ?";
+
+        try{
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setInt(1, stockId);
+
+            pstmt.executeUpdate();
+        } catch (Exception e){
+            System.out.println(e.toString());
+        }
     }
 
     public List<Stock> getStocks(String tickerQuery){
